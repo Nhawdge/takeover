e5b2e2e27182d2e95b1c305c2b34bd0c3e63987b
John <john.pavek@blendinteractive.com>
2021-08-22 22:38:43 -0500

    Adding Readme and clean up

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Raylib_cs;
+
+namespace Takeover {
+    class Program {
+        static void Main(string[] args) {
+            Raylib.InitWindow(800, 480, "Take Over");
+
+            Raylib.SetTargetFPS(30);
+            var game = new GameEngine();
+
+            while (!Raylib.WindowShouldClose()) {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+
+                game.GameLoop();
+
+                Raylib.EndDrawing();
+
+            }
+            Raylib.CloseWindow();
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Takeover 
+
+A clone of those games that are like this, but I don't know what they are called.
+
+## Comands for me to not forget
+
+dotnet publish -r win-x64 --self-contained true -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true
